d2bfab3a7b9593d71acdcce08f2ce476e523daf7
Arredondo-Juan <juan_arredondo@icloud.com>
2025-03-28 21:06:09 -0500

    Initial Commit

---
+++ ScreenTimeTest/ScreenTimeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTimeTestApp.swift
+//  ScreenTimeTest
+//
+//  Created by Juan Arredondo on 3/28/25.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTimeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
